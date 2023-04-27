bf645392b0487e13c208a2b8259572b935075627
Nat Allan <19149206+Truxnell@users.noreply.github.com>
2023-04-27 17:30:49 +1000

    fix: add debug

--- apps/yt-dlp/wrapper.sh
+++ apps/yt-dlp/wrapper.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 yt-dlp "$@"
-
+exit_code=$?
+echo "yt-dlp exit code $exit_code"
 # Map the application exit code to appropriate exit code for Kubernetes
 # Exit code 0 and 1 are mapped to 0 (success), and exit code 2 is mapped to 1 (warning)
-case $? in
+case $exit_code in
 0 | 1)
 	k8s_exit_code=0 # success
 	;;
@@ -15,6 +16,6 @@ case $? in
 	k8s_exit_code=2 # failure
 	;;
 esac
-
+echo "returnign k8s_exit code $k8s_exit_code"
 # Exit with the mapped exit code for Kubernetes
 exit $k8s_exit_code
